95bf6a31210c72ec7b2b7823ae7d15dbb03a06aa
Mike Dunham <mikedunham@Deekshas-Air.hitronhub.home>
2020-04-22 11:32:41 -0700

    Add stub for LandingPage. Setup routes for authorization. Update navbar.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,82 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useContext } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
 
 import Dashboard from './containers/Dashboard/Dashboard';
 import WatchCommute from './containers/WatchCommute/WatchCommute';
 import AuthorizationForm from './containers/AuthorizationForm/AuthorizationForm';
+import LandingPage from './containers/LandingPage/LandingPage';
 
-function App() {
+import { store } from './store/store';
+
+const App = () => {
+    const context = useContext(store);
+    const { state } = context;
+
+    let routes = (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+                <Route path="/" exact component={LandingPage} />
+                <Route path="/create-account" component={AuthorizationForm} />
+                <Route path="/login" component={AuthorizationForm} />
+                <Redirect to="/login" />
+            </Switch>
+        </Suspense>
+    );
+
+    if(state.token !== null) {
+        routes = (
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route path="/" exact component={Dashboard} />
+                    <Route path="/watch-commute" component={WatchCommute} />
+                    {/* ADD LOGOUT PAGE */}
+                    <Redirect to="/" />
+                </Switch>
+            </Suspense>
+        );
+    }
+
     return (
         <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             <nav className="navbar is-fixed-top has-background-grey-dark">
-                <div className="navbar-brand" style={{maxWidth: '850px', width: '100%', margin: 'auto'}}>
-                    <RouterLink className="navbar-item has-text-primary is-size-4 has-text-weight-bold" to="/">
-                        Caltrain Delay Watch
-                    </RouterLink>
-                    <RouterLink className="navbar-item has-text-white" to="/">
-                        Home
-                    </RouterLink>
+                <div style={{ maxWidth: '850px', width: '100%', margin: 'auto', display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}} >
+                    <div className="navbar-brand" 
+                        // style={{maxWidth: '850px', width: '100%', margin: 'auto'}}
+                        >
+                        <RouterLink className="navbar-item has-text-primary is-size-4 has-text-weight-bold" to="/">
+                            Caltrain Delay Watch
+                        </RouterLink>
+                        {state.token !== null 
+                            ?   <RouterLink className="navbar-item has-text-white is-hidden-mobile" to="/">
+                                    Home
+                                </RouterLink>
+                            :   null
+                        } 
+                    </div>
+                    {state.token !== null 
+                        ?   <div class="navbar-item">
+                                <RouterLink className="navbar-item has-text-white" to="/logout">
+                                    Log out
+                                </RouterLink>
+                            </div>
+                        : <div>
+                            <div class="navbar-item">
+                                <div class="buttons">
+                                <RouterLink class="button is-primary is-hidden-mobile" to="/create-account">
+                                    <strong>Sign up</strong>
+                                </RouterLink>
+                                <RouterLink class="button is-light" to="/login">
+                                    Log in
+                                </RouterLink>
+                                </div>
+                            </div>
+                        </div>
+                    }
                 </div>
             </nav>
             <div style={{ flexGrow: '1' }}>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Switch>
-                        <Route path="/" exact component={Dashboard} />
-                        <Route path="/watch-commute" component={WatchCommute} />
-                        <Route path="/create-account" component={AuthorizationForm} />
-                        <Route path="/login" component={AuthorizationForm} />
-                        <Redirect to="/" />
-                    </Switch>
-                </Suspense>
+                {routes}
             </div>
             <footer className="footer has-background-grey has-text-white" style={{ paddingBottom: '3rem' }}>
                 <div className="content has-text-centered">
