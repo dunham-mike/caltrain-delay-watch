13dd3a40210cbe58f4156d2beb2791c539c9445c
Mike Dunham <mikedunham@Deekshas-Air.hitronhub.home>
2020-04-29 15:12:02 -0700

    Update TrainWatched component to display status, if there is one.

--- src/containers/Dashboard/TrainWatched/TrainWatched.js
+++ src/containers/Dashboard/TrainWatched/TrainWatched.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
+import moment from 'moment-timezone';
+
 const TrainWatched = (props) => {
     let trainWatchedText = props.trainWatched.station 
         + (props.hasAddons ? ' Station: ' : ': ')
         + (props.trainWatched.direction === "NB" ? "NB" : "SB") + ' ' 
         + props.trainWatched.trainNumber + ' at ' + props.trainWatched.time;
+    
+    let trainStatusDisplay = null;
+
+    if(props.trainStatus) {
+        trainStatusDisplay = (
+            <div className="has-text-white has-text-centered">
+                <span className={"has-text-weight-bold " + (props.trainStatus.status !== "On Time" ? "has-text-primary" : "has-text-white")}>{props.trainStatus.status}</span> - Expected Departure: {moment.utc(props.trainStatus.expectedDepartureTime).tz('America/Los_Angeles').format('h:mm a')}
+            </div>
+        );
+    }
 
     return (
+        <React.Fragment>
         <div className={"field" + (props.hasAddons ? " has-addons" : "")} style={props.hasAddons ? {} : { display: 'flex', flexDirection: 'column', alignItems: 'center' } }>
             <p className="control">
                 <RouterLink to={{ 
@@ -37,6 +50,8 @@ const TrainWatched = (props) => {
                 </RouterLink>
             </p>
         </div>
+        {trainStatusDisplay}
+        </React.Fragment>
     );
 }
 
