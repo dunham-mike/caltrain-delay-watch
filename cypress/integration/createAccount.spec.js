6de500adee96b60655becf7ceb88e60560d91035
Mike Dunham <mldunham@gmail.com>
2020-05-05 20:38:18 -0700

    Finish Cypress integration tests.

--- cypress/integration/createAccount.spec.js
+++ cypress/integration/createAccount.spec.js
@@ -1,4 +1,33 @@
 describe('Create Account', () => {
+
+    const accountEmail = 'test@test.com';
+    const accountPassword = 'test_pass';
+    const phoneNumber = "2223334444";
+
+    beforeEach(() => {
+
+        // Fixtures
+        cy.fixture('loginSuccess.json').as('loginSuccessJSON');
+        cy.fixture('fetchUserDataSuccess.json').as('fetchUserDataSuccessJSON').then((userData) => {
+            userData.mostRecentNotifications[0].expectedDepartureTime = Cypress.moment.utc();
+        });
+        cy.fixture('fetchCurrentStatusSuccess.json').as('fetchCurrentStatusSuccessJSON').then((currentStatus) => {
+            currentStatus.createdAt = Cypress.moment.utc();
+        });
+
+        // Network stubs
+        cy.server();
+
+        const backendUrl = Cypress.env('backendUrl');
+
+        // GET
+        cy.route('GET', `${backendUrl}/api/user-data`, '@fetchUserDataSuccessJSON').as('fetchUserData');
+        cy.route('GET', `${backendUrl}/api/current-status`, '@fetchCurrentStatusSuccessJSON').as('fetchCurrentStatus');
+
+        // POST
+        cy.route('POST', `${backendUrl}/api/auth/create-account`, 'Account successfully created.').as('createAccount');
+        cy.route('POST', `${backendUrl}/api/auth/login`, '@loginSuccessJSON').as('submitLogin');
+    });
     
     it('LandingPage successfully loads', function() {
         cy.visit('/');
@@ -20,56 +49,138 @@ describe('Create Account', () => {
         cy.get('button.is-primary')
             .should('have.text', 'Create an account')
             .click();
+
+        cy.url().should('eq', Cypress.config().baseUrl + '/create-account');
+        
+        cy.get('div.is-size-5 div')
+            .should('have.text', 'Create Account');
+    });
+
+    it('Creates account with Text Message notification', function() {
+
+        cy.get('input').eq(0)
+            .type(accountEmail);
+        
+        cy.get('input').eq(1)
+            .type(accountPassword);
+        
+        cy.get('input').eq(4)
+            .type(phoneNumber);
+        
+        cy.get('button.is-success')
+            .should('have.text', 'Create Account')
+            .click();
+
+        cy.wait('@createAccount')
+            .its('requestBody').should('deep.eq', 
+                { 
+                    user: {
+                        email: accountEmail,
+                        password: accountPassword,
+                        preferredNotificationMethod: 'sms',
+                        phoneNumber: '+1' + phoneNumber                    
+                    }
+                });
+        
+        cy.wait('@submitLogin')
+            .its('requestBody').should('deep.eq', 
+                { 
+                    user: {
+                        email: accountEmail,
+                        password: accountPassword               
+                    }
+                });
+
+        cy.url().should('eq', Cypress.config().baseUrl + '/');
+
+        // Log out
+        cy.get('div.modal-content button')
+            .should('have.text', 'Close')
+            .click();
+
+        cy.get('div.navbar-burger')
+            .click();
+
+        cy.get('div.navbar-end a').eq(1)
+            .should('have.text', 'Log out')
+            .click();
+    });
+
+    it('Creates account with Web App Only notification', function() {
+        cy.get('div.buttons a.is-primary')
+            .should('have.text', 'Sign up')
+            .click();
+
+        cy.url().should('eq', Cypress.config().baseUrl + '/create-account');
         
         cy.get('div.is-size-5 div')
             .should('have.text', 'Create Account');
 
-        // TODO: Check other labels
+        cy.get('input').eq(0)
+            .type(accountEmail);
+        
+        cy.get('input').eq(1)
+            .type(accountPassword);
+        
+        cy.get('input').eq(3)
+            .click();
+        
+        cy.get('button.is-success')
+            .should('have.text', 'Create Account')
+            .click();
 
-        // TODO: Check button text
+        cy.wait('@createAccount')
+            .its('requestBody').should('deep.eq', 
+                { 
+                    user: {
+                        email: accountEmail,
+                        password: accountPassword,
+                        preferredNotificationMethod: 'web app'         
+                    }
+                });
+        
+        cy.wait('@submitLogin')
+            .its('requestBody').should('deep.eq', 
+                { 
+                    user: {
+                        email: accountEmail,
+                        password: accountPassword               
+                    }
+                });
+
+        cy.url().should('eq', Cypress.config().baseUrl + '/');
     });
 
-    it('Creates account with Web App Only notification', function() {
-        // TODO
-    });
+    // The below are probably better as unit tests in Jest/Enzyme
+    // it('Submit button is disabled until all fields are filled out', function() {
+    //     // TODO
+    // });
 
-    it('Creates account with Text Message notification', function() {
-        // TODO
-    });
+    // it('Phone number field is removed when Web App Only is selected', function() {
+    //     // TODO
+    // });
 
-    it('Redirects to the Dashboard when logged in', function() {
-        // TODO
-    });
+    // it('Phone number input is preserved when switching between Text Message and Web App Only', function () {
+    //     // TODO
+    // });
 
-    it('Submit button is disabled until all fields are filled out', function() {
-        // TODO
-    });
+    // it('Displays correct errors when missing required inputs', function() {
+    //     // TODO
+    // });
 
-    it('Phone number field is removed when Web App Only is selected', function() {
-        // TODO
-    });
+    // it('Displays correct errors when inputs are the wrong format', function() {
+    //     // TODO
+    // });
 
-    it('Phone number input is preserved when switching between Text Message and Web App Only', function () {
-        // TODO
-    });
+    // it('Stores phone numbers with +1 in front', function() {
+    //     // TODO
+    // });
 
-    it('Displays correct errors when missing required inputs', function() {
-        // TODO
-    });
+    // it('Displays a modal with an error message when an email address already has an account', function() {
+    //     // TODO
+    // });
 
-    it('Displays correct errors when inputs are the wrong format', function() {
-        // TODO
-    });
-
-    it('Stores phone numbers with +1 in front', function() {
-        // TODO
-    });
-
-    it('Displays a modal with an error message when an email address already has an account', function() {
-        // TODO
-    });
-
-    it('Bottom link redirects to the login page', function() {
-        // TODO
-    });
+    // it('Bottom link redirects to the login page', function() {
+    //     // TODO
+    // });
 });
--- cypress/integration/loggedInFunctionality.spec.js
+++ cypress/integration/loggedInFunctionality.spec.js
@@ -1,35 +1,36 @@
 describe('Logged In Functionality', () => {
 
+    const token = 'test_token';
+
     beforeEach(() => {
         
         // Fixtures
         cy.fixture('loginSuccess.json').as('loginSuccessJSON');
-        cy.fixture('fetchUserDataSuccess.json').as('fetchUserDataSuccessJSON');
-        cy.fixture('fetchCurrentStatusSuccess.json').as('fetchCurrentStatusSuccessJSON');
-
-        const email = 'test@test.com'; // should match fetchUserDataSuccess fixture
+        cy.fixture('fetchUserDataSuccess.json').as('fetchUserDataSuccessJSON').then((userData) => {
+            userData.mostRecentNotifications[0].expectedDepartureTime = Cypress.moment.utc();
+        });
+        cy.fixture('fetchCurrentStatusSuccess.json').as('fetchCurrentStatusSuccessJSON').then((currentStatus) => {
+            currentStatus.createdAt = Cypress.moment.utc();
+        });
+        cy.fixture('fetchTimetablesSuccess.json').as('fetchTimetablesSuccessJSON');
 
         // Network stubs
         cy.server();
 
         const backendUrl = Cypress.env('backendUrl');
 
-        // Authentication
-        cy.route('POST', `${backendUrl}/api/auth/login`, '@loginSuccessJSON').as('submitLogin');
-        
         // GET
         cy.route('GET', `${backendUrl}/api/user-data`, '@fetchUserDataSuccessJSON').as('fetchUserData');
         cy.route('GET', `${backendUrl}/api/current-status`, '@fetchCurrentStatusSuccessJSON').as('fetchCurrentStatus');
-        
-        // // PUT
-        // cy.route('PUT', `${backendUrl}/users/${userId}/sprints/*.json?auth=*`, {}).as('addOrUpdateSprint');
-        // cy.route('PUT', `${backendUrl}/users/${userId}/sprints/*/projects/*.json?auth=*`, {}).as('addOrUpdateProjectOnSprint');
-        // cy.route('PUT', `${backendUrl}/users/${userId}/queue/*.json?auth=*`, {}).as('addOrUpdateProjectOnQueue');
+        cy.route('GET', `${backendUrl}/api/timetables/caltrain/weekday`, '@fetchTimetablesSuccessJSON').as('fetchTimetables');
 
-        // // DELETE
-        // cy.route('DELETE', `${backendUrl}/users/${userId}/sprints/*.json?auth=*`, {}).as('deleteSprint');
-        // cy.route('DELETE', `${backendUrl}/users/${userId}/sprints/*/projects/*.json?auth=*`, {}).as('deleteProjectOnSprint');
-        // cy.route('DELETE', `${backendUrl}/users/${userId}/queue/*.json?auth=*`, {}).as('deleteProjectOnQueue');
+        // POST
+        cy.route('POST', `${backendUrl}/api/auth/login`, '@loginSuccessJSON').as('submitLogin');
+        cy.route('POST', `${backendUrl}/api/user-data`, {}).as('addWatchedTrain');
+        cy.route('POST', `${backendUrl}/api/user-data/preferences`, 'User preferences successfully updated.').as('updateSettings');
+
+        // DELETE
+        cy.route('DELETE', `${backendUrl}/api/user-data`, {}).as('deleteWatchedTrain');
     });
 
     it('LandingPage successfully loads', function() {
@@ -77,8 +78,6 @@ describe('Logged In Functionality', () => {
         cy.url().should('eq', Cypress.config().baseUrl + '/');
 
         // Wait for stubbed user data and current status responses
-        const token = 'test_token';
-
         cy.wait('@fetchUserData')
             .its('requestHeaders.Authorization').should('deep.eq', 
                 `Bearer ${token}`);
@@ -97,7 +96,7 @@ describe('Logged In Functionality', () => {
         
         // Dashboard loads correctly
         cy.get('div.is-size-5 div').eq(0)
-            .should('have.text', 'No Current Notifications');
+            .should('have.text', 'Current Notification');
     
         cy.get('div.is-size-5').eq(1)
             .should('have.text', 'Your AM Train');
@@ -106,19 +105,239 @@ describe('Logged In Functionality', () => {
             .should('have.text', 'Your PM Train');
     });
 
-    // TODO: Add AM train
+    it('Adds train for AM commute', function () {
 
-    // TODO: See Notification and Status for AM train
+        // Navigate to AM Watch Commute page
+        cy.get('button.is-warning')
+            .should('have.text', 'Select a morning train to watch!')
+            .click();
+        
+        cy.url().should('eq', Cypress.config().baseUrl + '/watch-commute');
 
-    // TODO: Delete AM train
+        cy.wait('@fetchTimetables')
+            .its('requestHeaders.Authorization').should('deep.eq', 
+                `Bearer ${token}`);
 
-    // TODO: Add PM train (click more trains)
-    
-    // TODO: Change PM train
+        cy.get('div.is-size-5').eq(1)
+            .should('have.text', 'Update AM Commute');
 
-    // TODO: Update settings
+        // Select AM train
+        cy.get('select')
+            .select('Burlingame');
 
-    // TODO: Refresh button
+        cy.get('div.field.has-addons button span').eq(1)
+            .should('have.text', 'Northbound')
+            .click();
+        
+        cy.get('button.is-info.is-outlined').eq(4)
+            .should('have.text', '8:30 am - NB 221')
+            .click();
 
-    // TODO: Log out
+        // Wait for stubbed POST request
+        cy.get('@addWatchedTrain').should((req) => {
+            expect(req.request.headers.Authorization).to.deep.equal(`Bearer ${token}`);
+
+            expect(req.request.body).to.deep.equal({
+                commuteType: 'AM',
+                trainInfo: {
+                    operator: 'Caltrain',
+                    scheduleType: 'Weekday',
+                    station: 'Burlingame',
+                    stopId: '70081',
+                    direction: 'NB',
+                    time: '8:30 am',
+                    trainNumber: '221'
+                }
+            });
+        });
+            
+        // Redirect to Dashboard and display added train
+        cy.url().should('eq', Cypress.config().baseUrl + '/');
+
+        cy.get('button.is-light.is-warning').eq(0)
+            .should('have.text', 'Burlingame Station: NB 221 at 8:30 am');
+    });
+
+    it('Displays notifications and current status for AM train', function () {
+        cy.get('div.has-text-white.has-text-centered')
+            .contains('Late - Expected Departure: 8:45 am');
+        
+        cy.get('div.content div.has-text-centered')
+            .should('have.text', 'Delayed: NB 221 at Burlingame Station');
+    });
+
+    it('Deletes the AM train', function () {
+        cy.get('button.is-light.is-warning')
+            .contains('Burlingame Station: NB 221 at 8:30 am')
+            .click();
+        
+        cy.get('button.is-primary.is-outlined span').eq(1)
+            .should('have.text', 'Or Delete Current Train')
+            .click();
+        
+        // Wait for stubbed DELETE request
+        cy.get('@deleteWatchedTrain').should((req) => {
+            expect(req.request.headers.Authorization).to.deep.equal(`Bearer ${token}`);
+            expect(req.request.body).to.deep.equal({
+                commuteType: 'AM'
+            });
+        });
+        
+        cy.url().should('eq', Cypress.config().baseUrl + '/');
+    });
+
+    it('Adds a PM train', function () {
+
+        // Navigate to PM Watch Commute page
+        cy.get('button.is-info').eq(1)
+            .should('have.text', 'Select an evening train to watch!')
+            .click();
+        
+        cy.url().should('eq', Cypress.config().baseUrl + '/watch-commute');
+
+        cy.get('div.is-size-5').eq(1)
+            .should('have.text', 'Update PM Commute');
+
+        // Select PM train
+        cy.get('select')
+            .select('San Francisco');
+
+        cy.get('div.field.has-addons button span').eq(2)
+            .should('have.text', 'Southbound')
+            .click();
+
+
+        cy.get('button.is-warning').eq(1)
+            .should('have.text', 'Show More Times')
+            .click();
+        
+        cy.get('button.is-info.is-outlined').eq(9)
+            .should('have.text', '8:30 pm - SB 192')
+            .click();
+
+        // Wait for stubbed POST request
+        cy.get('@addWatchedTrain').should((req) => {
+            expect(req.request.headers.Authorization).to.deep.equal(`Bearer ${token}`);
+
+            expect(req.request.body).to.deep.equal({
+                commuteType: 'PM',
+                trainInfo: {
+                    operator: 'Caltrain',
+                    scheduleType: 'Weekday',
+                    station: 'San Francisco',
+                    stopId: '70012',
+                    direction: 'SB',
+                    time: '8:30 pm',
+                    trainNumber: '192'
+                }
+            });
+        });
+            
+        // Redirect to Dashboard and display added train
+        cy.url().should('eq', Cypress.config().baseUrl + '/');
+
+        cy.get('button.is-light.is-info').eq(0)
+            .should('have.text', 'San Francisco Station: SB 192 at 8:30 pm');
+    });
+
+    it('Updates an existing PM train', function () {
+
+        // Navigate to PM Watch Commute page
+        cy.get('button.is-light.is-info').eq(0)
+            .click();
+        
+        cy.url().should('eq', Cypress.config().baseUrl + '/watch-commute');
+
+        // Select PM train
+        cy.get('select')
+            .select('San Bruno');
+
+        cy.get('div.field.has-addons button span').eq(2)
+            .should('have.text', 'Southbound')
+            .click();
+        
+        cy.get('button.is-info.is-outlined').eq(3)
+            .should('have.text', '3:20 pm - SB 156')
+            .click();
+
+        // Wait for stubbed POST request
+        cy.get('@addWatchedTrain').should((req) => {
+            expect(req.request.headers.Authorization).to.deep.equal(`Bearer ${token}`);
+
+            expect(req.request.body).to.deep.equal({
+                commuteType: 'PM',
+                trainInfo: {
+                    operator: 'Caltrain',
+                    scheduleType: 'Weekday',
+                    station: 'San Bruno',
+                    stopId: '70052',
+                    direction: 'SB',
+                    time: '3:20 pm',
+                    trainNumber: '156'
+                }
+            });
+        });
+            
+        // Redirect to Dashboard and display updated train
+        cy.url().should('eq', Cypress.config().baseUrl + '/');
+
+        cy.get('button.is-light.is-info').eq(0)
+            .should('have.text', 'San Bruno Station: SB 156 at 3:20 pm');
+    });
+
+    it('Updates settings', function () {
+        cy.get('div.navbar-burger')
+            .click();
+
+        cy.get('div.navbar-end a').eq(0)
+            .should('have.text', 'Settings')
+            .click();
+        
+        cy.url().should('eq', Cypress.config().baseUrl + '/settings');
+
+        cy.get('label.radio').eq(1)
+            .should('have.text', 'Web App Only')
+            .click();
+
+        cy.get('button.is-success')
+            .should('have.text', 'Update Settings')
+            .click();
+        
+        // Wait for stubbed POST request
+        cy.get('@updateSettings').should((req) => {
+            expect(req.request.headers.Authorization).to.deep.equal(`Bearer ${token}`);
+
+            expect(req.request.body).to.deep.equal({
+                preferredNotificationMethod: 'web app',
+                phoneNumber: null
+            });
+        });
+
+        cy.url().should('eq', Cypress.config().baseUrl + '/');
+    });
+
+    it('Displays a modal when attempting to refresh', function () {
+        cy.get('button.is-small')
+            .click();
+
+        cy.get('div.modal-content div.box')
+            .contains('New status updates are available every 5 minutes on weekdays. Please try again after');
+
+        cy.get('div.modal-content button')
+            .should('have.text', 'Close')
+            .click();
+    });
+
+    it('Logs out successfully', function () {
+        cy.get('div.navbar-burger')
+            .click();
+
+        cy.get('div.navbar-end a').eq(1)
+            .should('have.text', 'Log out')
+            .click();
+        
+        // LandingPage content renders
+        cy.get('div.is-size-3')
+            .should('have.text', 'Your commute just became a lot more reliable');
+    });
 });
